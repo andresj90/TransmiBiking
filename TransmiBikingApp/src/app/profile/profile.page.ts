629a87182a0b960e5f3e8c7362c34b566e972988
andres <felipetk190@gmail.com>
2019-02-23 00:15:46 -0500

    Created Profile page
    for the profile page We generated an object user on the controller to simulate the users profile on the view.

--- TransmiBikingApp/src/app/profile/profile.page.ts
+++ TransmiBikingApp/src/app/profile/profile.page.ts
@@ -7,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfilePage implements OnInit {
 
+  user = {
+    name: 'Anatolio',
+    lastName: 'Rios', 
+    userName: 'toliori22',
+    loans: 7,
+    lastLoan: new Date(2019,2,11),
+    profilePic: '/assets/icon/man.svg'
+  }
+
   constructor() { }
 
   ngOnInit() {
