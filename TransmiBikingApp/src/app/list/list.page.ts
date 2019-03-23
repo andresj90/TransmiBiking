8ad53a7cf95826fef68f2751d4059893e72098d8
jaracodes <felipetk190@gmail.com>
2019-03-23 03:14:12 -0500

    Firebase persistance added on reservation
    Firebase persistance added to the reservation

--- TransmiBikingApp/src/app/list/list.page.ts
+++ TransmiBikingApp/src/app/list/list.page.ts
@@ -8,19 +8,19 @@ import { NativeStorage } from '@ionic-native/native-storage/ngx';
   styleUrls: ['list.page.scss']
 })
 export class ListPage implements OnInit {
-  reserva =
-    {
-      dateReservation: '',
-      originReservation: '',
-      bykeType: ''
-    }
+  
+  public dateReservation : String;
+  public origin: String;
+  public bikeType: String; 
+  public returnStation: String; 
+  public time: String;
 
   constructor(private nativeStorage: NativeStorage) { }
 
   generateBooking() {
-    window.alert(`Se ha generado una reserva para ${this.reserva.dateReservation}
-    con origen en ${this.reserva.originReservation} y tipo de bicicleta para ${this.reserva.bykeType}`);
-    this.nativeStorage.setItem('reserva', this.reserva)
+    window.alert(`Se ha generado una reserva para ${this.dateReservation}
+    con origen en ${this.origin} y tipo de bicicleta para ${this.bikeType}`);
+    this.nativeStorage.setItem('reserva', {reserva: this.dateReservation})
       .then(
         () => console.log('Stored item!'),
         error => console.error('Error storing item', error)
--- TransmiBikingApp/src/app/prestamo/prestamo.page.ts
+++ TransmiBikingApp/src/app/prestamo/prestamo.page.ts
@@ -6,30 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./prestamo.page.scss'],
 })
 export class PrestamoPage implements OnInit {
+   
+    stations = ['A','B','C','D','E','F'];
+    hours = [2,3,4,5,6,7,8,9];
 
-  prestamos = [{
-    date: new Date(2018, 8, 22),
-    duration: '2 hours',
-    origin: 'station A',
-    return: 'station C'
-  }, {
-    date: new Date(2018, 9, 12),
-    duration: '4 hours',
-    origin: 'station D',
-    return: 'station G'
-  }, {
-    date: new Date(2018, 11, 22),
-    duration: '5 hours',
-    origin: 'station B',
-    return: 'station E'
-  }, {
-    date: new Date(2019, 1, 17),
-    duration: '7 hours',
-    origin: 'station A',
-    return: 'station H'
-  }]
+   prestamos = [
+     {
+      dateReservation : '',
+      origin: '',
+      bikeType: '',
+      returnStation: `Estación ${this.stations[Math.floor(Math.random()*this.stations.length)]}`,
+      time: `${this.hours[Math.floor(Math.random()*this.hours.length)]} Horas`
+     }
+   ]
 
   constructor() { }
+
+  ObtenerPrestamos(){
+
+  }
 
   ngOnInit() {
   }
