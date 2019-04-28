69e0334a75ff16a35e6ceef4cf73f936f528bd09
DavidUK <36526045+c-esar@users.noreply.github.com>
2019-04-28 02:57:21 -0500

    The panel was created in page Form person
    The panel was created for the creation part of the office for the entrance to the platform

--- TransmiBikingApp/src/app/form-person/form-person.module.ts
+++ TransmiBikingApp/src/app/form-person/form-person.module.ts
@@ -6,7 +6,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 
 import { FormPersonPage } from './form-person.page';
-
+import { PanelModule } from 'primeng/panel';
 const routes: Routes = [
   {
     path: '',
@@ -19,6 +19,7 @@ const routes: Routes = [
     CommonModule,
     FormsModule,
     IonicModule,
+    PanelModule,
     RouterModule.forChild(routes)
   ],
   declarations: [FormPersonPage]
