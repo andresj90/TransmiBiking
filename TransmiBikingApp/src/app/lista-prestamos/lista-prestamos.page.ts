d4562bb1060d287112592ba0b3df50c02e7c1e60
DavidUK <36526045+c-esar@users.noreply.github.com>
2019-04-28 21:39:00 -0500

    The loan list is created
    It creates the user's loans are reflected in a table that can be viewed.

--- TransmiBikingApp/src/app/lista-prestamos/lista-prestamos.page.ts
+++ TransmiBikingApp/src/app/lista-prestamos/lista-prestamos.page.ts
@@ -9,33 +9,42 @@ export class ListaPrestamosPage implements OnInit {
   stations = ['Cesar', 'B', 'C', 'D', 'Diana'];
   hours = [2, 3, 4, 5, 6, 7, 8, 9];
   public colum: any[];
-  public prestamo = [];
+  prestamo: any;
 
   constructor(public prestamoAuth: PrestamoService) {
   }
 
   ngOnInit() {
     this.MostrarColumnas();
+    this.Variables();
+    this.MostrarPrestamo();
   }
   MostrarColumnas() {
     this.colum = [
-      {  header: 'Numero'},
-      {  header: 'Fecha'},
-      {  header: 'Origin'},
-      {  header: 'bikeType'}
+      { header: 'Numero' },
+      { header: 'Fecha' },
+      { header: 'Origin' },
+      { header: 'bikeType' }
     ];
   }
-  MostrarPrestamo(id) {
-    // trae todos los comentarios
-//    this.prestamoAuth.ObtenerPrestamo(id).subscribe((prestamo) => {
-//      this.prestamo = [];
-//      prestamo.forEach((prestamodata: any) => {
-//        this.prestamo.push({
-//          id: prestamodata.payload.doc.id,
-//          data: prestamodata.payload.doc.data()
-//        });
-//      });
-//    });
+  Variables() {
+    this.prestamo = {
+      fecha: '',
+      ruta: '',
+      adulto: '',
+      nino: ''
+    };
+  }
+  MostrarPrestamo() {
+    this.prestamoAuth.ObtenerPrestamo().subscribe((prestamo) => {
+      this.prestamo = [];
+      prestamo.forEach((prestamodata: any) => {
+        this.prestamo.push({
+          id: prestamodata.payload.doc.id,
+          data: prestamodata.payload.doc.data()
+        });
+      });
+    });
   }
 
 }
